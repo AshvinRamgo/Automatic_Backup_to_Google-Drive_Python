179b11145a66ddacb1d29b1c3d6a10506807f89f
a2ramgoo <a2ramgoo@uwaterloo.ca>
2025-01-09 17:33:42 -0500

    updated-to-new-google-format

--- quickstart.py
+++ quickstart.py
@@ -1,66 +1,76 @@
-import os.path
-from dotenv import load_dotenv
+import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build, MediaFileUpload
+from googleapiclient.errors import HttpError
+from dotenv import load_dotenv
+import google.auth.exceptions  
 
-class MyDrive(): 
-  def __init__(self): 
-    SCOPES = ["https://www.googleapis.com/auth/drive"]
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+class MyDrive():
+  def __init__(self):
     creds = None
-    if os.path.exists("token.json"):
-      creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists('token.json'):
+      creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     if not creds or not creds.valid:
       if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-      else:
+        try:
+          creds.refresh(Request())
+        except google.auth.exceptions.RefreshError:
+          creds = None
+      if not creds:
         flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-        )
+            'credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-    self.service = build("drive", "v3", credentials=creds)
+        with open('token.json', 'w') as token:
+          token.write(creds.to_json())
+    self.service = build('drive', 'v3', credentials=creds)
 
   def list_files(self, page_size=10):
-    results = (
-      self.service.files()
-      .list(pageSize=page_size, fields="nextPageToken, files(id, name)")
-      .execute()
-    )
-    items = results.get("files", [])
-    if not items:
-      print("No files found.")
-    else:
-      print("Files:")
-      for item in items:
-        print(f"{item['name']} ({item['id']})")
-    
+    try:
+      results = (
+          self.service.files()
+          .list(pageSize=page_size, fields="nextPageToken, files(id, name)")
+          .execute()
+      )
+      items = results.get("files", [])
+      if not items:
+        print("No files found.")
+      else:
+        print("Files:")
+        for item in items:
+          print(f"{item['name']} ({item['id']})")
+    except HttpError as error:
+      print(f"An error occurred: {error}")
+
   def upload_file(self, filename, path):
-    folder_id = os.getenv('FOLDER_ID')
-    media = MediaFileUpload(os.path.join(path, filename))
-    response = self.service.files().list(
-      q=f"name='{filename}' and parents='{folder_id}'",
-      spaces='drive',
-      fields='nextPageToken, files(id,name)',
-      pageToken=None
-    ).execute()
-    if len(response['files']) == 0:
-      file_metadata = {
-        'name': filename,
-        'parents' : [folder_id]
-      }
-      file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-      print(f"A new File was created {file.get('id')}")
-    else:
-      for file in response.get('files', []):
-        update_file = self.service.files().update(
-          fileId=file.get('id'),
-          media_body=media,
-        ).execute()
-        print(f'Updated File')
+    try:
+      folder_id = os.getenv('FOLDER_ID')
+      media = MediaFileUpload(os.path.join(path, filename))
+      response = self.service.files().list(
+        q=f"name='{filename}' and parents='{folder_id}'",
+        spaces='drive',
+        fields='nextPageToken, files(id,name)',
+        pageToken=None
+      ).execute()
+      if len(response['files']) == 0:
+        file_metadata = {
+          'name': filename,
+          'parents' : [folder_id]
+        }
+        file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print(f"A new File was created {file.get('id')}")
+      else:
+        for file in response.get('files', []):
+          update_file = self.service.files().update(
+            fileId=file.get('id'),
+            media_body=media,
+          ).execute()
+          print(f'Updated File')
+    except HttpError as error:
+      print(f"An error occurred: {error}")
 
 load_dotenv()
 
